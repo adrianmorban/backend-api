ef8fb797ac1e320a4eaecef547848078ec466832
Adrian <adrianmorban@outlook.com>
2021-10-20 16:01:26 -0400

    first commit

---
+++ routes/api.php
@@ -0,0 +1,19 @@
+<?php
+
+use App\Http\Controllers\AuthController;
+use App\Http\Controllers\BlogController;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| API Routes
+|--------------------------------------------------------------------------
+*/
+
+Route::post('login', [AuthController::class, 'signin']);
+Route::post('register', [AuthController::class, 'signup']);
+     
+Route::middleware('auth:sanctum')->group( function () {
+    Route::resource('blogs', BlogController::class);
+});
